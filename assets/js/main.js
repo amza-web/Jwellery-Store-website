abf0042ab93c490d14fd64b1ec1e9a4c8c6d6e61
Amza <amza.web@gmail.com>
2023-05-07 04:36:44 +0100

    Finished the HomePage Project for the Mangata and Gallo Jwellery Store

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,16 @@ btnOpen.onclick = ()=> {
 btnClose.onclick = ()=> {
     nav.classList.remove("open")
 }
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        entry.target.style.animationDelay = `${index * 0.4}s`;
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+  
+  const introAnimationElements = document.querySelectorAll('.intro-animation');
+  introAnimationElements.forEach(el => observer.observe(el));
